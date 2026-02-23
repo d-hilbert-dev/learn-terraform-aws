5010e6249ca5732291278d8d0fac6f1612d0ada8
d-hilbert-dev <denis.hilbert.muc@gmail.com>
2026-02-23 19:56:55 +0100

    Initial Commit Terraform Up & Running

--- main.tf
+++ main.tf
@@ -1,24 +1,8 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-central-1"
 }
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
-  }
-
+resource "aws_instance" "example" {
+  ami           = "ami-0786adace1541ca80"
+  instance_type = "t3.micro"
+  
 }
