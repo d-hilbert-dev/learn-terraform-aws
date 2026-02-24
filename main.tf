316f648348110b4cb3b67b4be10682d7164684b3
d-hilbert-dev <denis.hilbert.muc@gmail.com>
2026-02-24 21:18:18 +0100

    Updated Up & Running Chapter 2 - ALB,ASG

--- main.tf
+++ main.tf
@@ -1,8 +1,132 @@
 provider "aws" {
-  region = "eu-central-1"
+region = "eu-central-1"
 }
-resource "aws_instance" "example" {
-  ami           = "ami-0786adace1541ca80"
+resource "aws_launch_template" "example" {
+  image_id = "ami-01f79b1e4a5c64257"
   instance_type = "t3.micro"
-  
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+user_data = base64encode(<<-EOF
+            #!/bin/bash
+            echo "Hello, World!" > index.html
+            nohup busybox httpd -f -p 8080 &
+            EOF
+)
+# Required when using a launch template with an Auto Scaling group.
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_security_group" "instance" {
+  name        = "terraform-example_instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+variable "server_port" {
+  description = "HTTP Request Port"
+  type = number
+  default = 8080
+}
+data "aws_vpc" "default" {
+  default = true
+}
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+resource "aws_autoscaling_group" "example" {
+  launch_template {
+    id = aws_launch_template.example.id
+  }
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+resource "aws_lb" "example" {
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnets.default.ids
+  security_groups = [aws_security_group.alb.id]
+}
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = "80"
+  protocol = "HTTP"
+  #By default, return a simple 404 message
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: Not Found"
+      status_code = "404"
+    }
+  }
+}
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  #Allows inbound HTTP requests from anywhere
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
